c382cf2ebed090e7d06259cb3439e2d71373e243
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-02 23:17:47 +0100

    🚀 Spread Operator(Objects and Functions)

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-// ! let's say we want to combin three or whatever arrays in one array without make it nested array
-let arrayOne = [1, 2, 3, 4];
-let arrayTwo = ["Jhon Doe", "Maria Cartner"];
-let number = 123;
-// * first way
-let myArrays = [].concat(arrayOne, arrayTwo, number);
-console.log(myArrays); // [1, 2, 3, 4, 'Jhon Doe', 'Maria Cartner', 123]
-// * Spread Operator way
-let myArrays2 = [...arrayOne, ...arrayTwo, number];
-console.log(myArrays2); // [1, 2, 3, 4, 'Jhon Doe', 'Maria Cartner', 123]
-//  ? with Spread Operator is look clean
+// ! Spread Operator
+// * Object
+/* 
+? object-literal {}) => passed by reference
+? mean: if you create a copy of variable and change that copy, the main variable it will change also
+*/
+
+const studentInfo = {
+  fullName: "Jhon Doe",
+  age: 26,
+};
+console.log(studentInfo);
+let copyOfStudentInfo = studentInfo;
+copyOfStudentInfo.fullName = "Maria Cartner";
+console.log(studentInfo); // {fullName: "Maria Cartner", age: 26}
+console.log("########################################################");
+// * if you want the main variable does not change, use spread operator
+const studentInfo2 = {
+  fullName: "Steven Doe",
+  age: 22,
+};
+console.log(studentInfo2); // {fullName: 'Steven Doe', age: 22}
+let copyOfStudentInfo2 = { ...studentInfo2 };
+copyOfStudentInfo2.fullName = "Andres Doe";
+console.log(copyOfStudentInfo2); // {fullName: 'Andres Doe', age: 22}
+console.log(studentInfo2); // {fullName: 'Steven Doe', age: 22}
+// ! as you can see value of main variable does not change by using spread operator
+
+// * Function
+
+const addNumbers = (a, b, c) => a + b + c;
+const numbers = [10, 20, 30];
+const result = addNumbers(...numbers);
+console.log(result);
