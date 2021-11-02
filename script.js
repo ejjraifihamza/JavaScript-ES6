903ffcd5cf5855d97a5046a64dacb4f9a325ef32
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-02 22:26:02 +0100

    🚀 Primitive passed by value, Objects by reference

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-// ! Array.some() check if one element in array give a true value, so it will give a true even if other elements give false
+// ! Data type:
+/*
+? Primitive types (string, number, boolean, undefined, null) => passed by value
+? mean: even if you create a copy of variable and change that copy, the main variable does not change
+*/
+
+let fullName = "Jhon Doe";
+let compFullName = fullName;
+compFullName = "Maria Cartner";
+console.log(fullName); // Jhon Doe => primtive types: passed by value
 
 /*
-? arg: some(element, index, arrayItSelf) 
-! function should return boolean
+? Object (array-literal [], object-literal {}) => passed by reference
+? mean: if you create a copy of variable and change that copy, the main variable it will change also
 */
-// * check if number in array numbers is greather than twenty
-const numbers = [3, 5, 15, 27, 31];
-const isGreatherThanTwenty = numbers.some((element, index, arrayItSelf) => {
-  return element > 20;
-});
-console.log(isGreatherThanTwenty); // return true
-// * check if number in array numbers is double
-const isEven = numbers.some((number, index, arrayItSelf) => {
-  return number % 2 == 0;
-});
-console.log(isEven); // return false
+// * array
+let numbers = [1, 2, 3, 4, 5];
+let copyOfNumbers = numbers;
 
-// ! Array.every() check if all element in array give a true/false value
-/*
-? to make it return true should every element in array passe the test with true
-? if one element does not passe the test it will return false
-*/
-const myNumbers = [5, 6, 9, 54, 66];
-const isGreatherThanTen = myNumbers.every((element, index, arrayItSelf) => {
-  return element > 10;
-});
-console.log(isGreatherThanTen); // return false
+copyOfNumbers[0] = 100;
+console.log(numbers); // [100, 2, 3, 4, 5] => array is passed by reference
 
-const isGreatherThanFour = myNumbers.every((element, index, arrayItSelf) => {
-  return element > 4;
-});
-console.log(isGreatherThanFour); // return true
+// * object
+let studentInfo = {
+  fullName: "Jhon Doe",
+  age: 26,
+};
+console.log(studentInfo);
+let copyOfStudentInfo = studentInfo;
+copyOfStudentInfo.fullName = "Maria Cartner";
+console.log(studentInfo); // {fullName: "Maria Cartner", age: 26}
