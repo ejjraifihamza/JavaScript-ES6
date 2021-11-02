81110eff16162986e5d14b5d6e6ce24c20355416
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-02 22:42:52 +0100

    🚀 Spread Operator (Arrays)

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-// ! Data type:
-/*
-? Primitive types (string, number, boolean, undefined, null) => passed by value
-? mean: even if you create a copy of variable and change that copy, the main variable does not change
-*/
-
-let fullName = "Jhon Doe";
-let compFullName = fullName;
-compFullName = "Maria Cartner";
-console.log(fullName); // Jhon Doe => primtive types: passed by value
-
-/*
-? Object (array-literal [], object-literal {}) => passed by reference
-? mean: if you create a copy of variable and change that copy, the main variable it will change also
-*/
-// * array
-let numbers = [1, 2, 3, 4, 5];
-let copyOfNumbers = numbers;
-
-copyOfNumbers[0] = 100;
-console.log(numbers); // [100, 2, 3, 4, 5] => array is passed by reference
-
-// * object
-let studentInfo = {
-  fullName: "Jhon Doe",
-  age: 26,
-};
-console.log(studentInfo);
-let copyOfStudentInfo = studentInfo;
-copyOfStudentInfo.fullName = "Maria Cartner";
-console.log(studentInfo); // {fullName: "Maria Cartner", age: 26}
+// ! let's say we want to combin three or whatever arrays in one array without make it nested array
+let arrayOne = [1, 2, 3, 4];
+let arrayTwo = ["Jhon Doe", "Maria Cartner"];
+let number = 123;
+// * first way
+let myArrays = [].concat(arrayOne, arrayTwo, number);
+console.log(myArrays); // [1, 2, 3, 4, 'Jhon Doe', 'Maria Cartner', 123]
+// * Spread Operator way
+let myArrays2 = [...arrayOne, ...arrayTwo, number];
+console.log(myArrays2); // [1, 2, 3, 4, 'Jhon Doe', 'Maria Cartner', 123]
+//  ? with Spread Operator is look clean
