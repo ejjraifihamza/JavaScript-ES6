6baf0deeeef57fd1b438a644008cb9ac6cbf729a
ejjraifihamza <ejjraifihamza@gmail.com>
2021-11-03 21:37:23 +0100

    🚀 Modules

---
+++ arithmetic.js
@@ -0,0 +1,19 @@
+/*
+ * Addition
+ */
+export const add = (x, y) => parseFloat(x) + parseFloat(y);
+
+/*
+ * Multiplication
+ */
+const mul = (x, y) => parseFloat(x) * parseFloat(y);
+
+/*
+ * Division
+ */
+const divide = (x, y) => parseFloat(x) / parseFloat(y);
+
+/*
+ * Substraction
+ */
+export const sub = (x, y) => parseFloat(x) - parseFloat(y);
---
+++ calculator.js
@@ -0,0 +1,8 @@
+// Calculator
+
+// Addition, Subtraction
+// Division, Multiplication
+// arithmetic.js
+import { add, sub } from "./arithmetic.js";
+console.log(add(5, 5));
+console.log(sub(10, 5));
